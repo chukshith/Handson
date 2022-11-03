938c1e3a581ea4f1d0c38e484427a36eb2051db8
chukshith <53993293+chukshith@users.noreply.github.com>
2022-11-03 15:25:28 +0530

    Add files via upload

---
+++ H1.cs
@@ -0,0 +1,19 @@
+﻿using system;
+
+namespace Hands_on
+{
+    internal class _1
+    {
+        static void main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter the name :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your Age :");
+            age = int.parse(Console.ReadLine());
+            Console.WriteLine("Enter your country :");
+            string country = Console.ReadLine();
+            Console.WriteLine($"welocome {name}. Your age is {age} and you are from {country}");
+        }
+    }
+}
